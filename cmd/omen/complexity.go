ae64f8698186f3fcc195369ec7e9a02825b26047
Jonathan Reyes <me@jonathanreyes.com>
2025-12-08 12:09:12 -0700

    feat(complexity): integrate remote repo support

--- cmd/omen/complexity.go
+++ cmd/omen/complexity.go
@@ -28,7 +28,15 @@ func init() {
 }
 
 func runComplexity(cmd *cobra.Command, args []string) error {
-	paths := getPaths(args)
+	ref, _ := cmd.Flags().GetString("ref")
+	shallow, _ := cmd.Flags().GetBool("shallow")
+
+	paths, cleanup, err := resolvePaths(cmd.Context(), args, ref, shallow)
+	if err != nil {
+		return err
+	}
+	defer cleanup()
+
 	cycThreshold, _ := cmd.Flags().GetInt("cyclomatic-threshold")
 	cogThreshold, _ := cmd.Flags().GetInt("cognitive-threshold")
 	functionsOnly, _ := cmd.Flags().GetBool("functions-only")
